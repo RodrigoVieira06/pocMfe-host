6cf640878857abaf34dc0acc85aec02176a08e39
Rodrigo Lima <rodrigo.lima.3@globalhitss.com.br>
2025-02-10 17:10:21 -0300

    fix: fixing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const RemoteLogin = lazy(() => import("loginApp/LoginPage"));
 const RemoteHome = lazy(() => import("homeApp/HomePage"));
@@ -8,9 +8,8 @@ const App: React.FC = () => {
   return (
     <Suspense fallback={<div>Carregando...</div>}>
       <Routes>
-        <Route path="/login" element={<RemoteLogin />} />
-        <Route path="/*" element={<RemoteHome />} />
-        <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/login" element={<RemoteLogin />} />
+      <Route path="/*" element={<RemoteHome />} />
       </Routes>
     </Suspense>
   );
